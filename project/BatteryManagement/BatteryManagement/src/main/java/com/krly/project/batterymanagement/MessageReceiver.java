9be28f9d1230e003f5def0a17f37996459ec6461
superpangdudu <1659929144@qq.com>
2018-05-22 14:13:07 +0800

    update
    update

---
+++ project/BatteryManagement/BatteryManagement/src/main/java/com/krly/project/batterymanagement/MessageReceiver.java
@@ -0,0 +1,38 @@
+package com.krly.project.batterymanagement;
+
+import kafka.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Properties;
+
+@Component
+public class MessageReceiver {
+    
+    @PostConstruct
+    public void start() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        //props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("battery"));
+
+        new Thread( () -> {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+        }) {
+
+        }.start();
+    }
+}
